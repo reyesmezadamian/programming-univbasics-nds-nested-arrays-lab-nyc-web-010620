0914ea2e0f5ca7b1211fdcb12615bd079b061100
Damian Reyes <reyesmezadamian@gmail.com>
2019-12-20 17:57:29 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,8 +26,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-matrix_lookup = assembled_matrix[1][1]
-return matrix_lookup
+matrix [row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
